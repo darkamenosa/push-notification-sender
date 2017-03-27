7097425ea6cf6a4e69454b0de5ea38710d4924ec
tuyenhx <hxtxmu@gmail.com>
2017-03-28 00:03:37 +0700

    Initialize commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+config = {
+	'name': 'pushnoti',
+	'version': '0.1.0',
+	'packages': ['pushnoti'],
+	'author': 'darkamenosa',
+	'author_email': 'hxtxmu@gmail.com',
+	'url': '',
+	'description': 'My first python program, a website to test push notification [APNS, GCM]',
+	'classifiers': [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'License :: OSI Approved :: MIT License',
+		'Environment :: Web Environment',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: POSIX :: Linux',
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Topic :: Utilities'
+	]
+}
+
+setup(**config)
